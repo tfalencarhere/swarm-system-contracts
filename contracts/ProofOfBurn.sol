3a7a7c5e74d60765f8988f3f27c163cb4428c156
Ralph Pichler <pichler.ralph@gmail.com>
2019-11-18 17:26:48 +0100

    move the ERC20 mintable dependancy to Migrations

--- contracts/ProofOfBurn.sol
+++ contracts/ProofOfBurn.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.5.13;
-import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ProofOfBurn {
   using SafeMath for uint256;
